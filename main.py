da8edf728570ff644c2d98eed5866534b44f2b8d
PNKlethal <seregaa99@bk,ru>
2022-12-02 20:45:34 +0600

    5 задание

--- main.py
+++ main.py
@@ -52,3 +52,22 @@ for element in range(my_string.count(' ') + 1):
     else:
         print(f" {number} {my_word [element] [0:10]}")
         number += 1
+
+#5 задание. Реализовать структуру «Рейтинг», представляющую собой не возрастающий набор натуральных чисел. У пользователя необходимо запрашивать новый элемент рейтинга. Если в рейтинге существуют элементы с одинаковыми значениями, то новый элемент с тем же значением должен разместиться после них.
+
+my_list = [7, 5, 3, 3, 2]
+print(f"Рейтинг - {my_list}")
+digit = int(input("Введите число (555 -> выход) "))
+while digit != 555:
+    for el in range(len(my_list)):
+        if my_list[el] == digit:
+            my_list.insert(el + 1, digit)
+            break
+        elif my_list[0] < digit:
+            my_list.insert(0, digit)
+        elif my_list[-1] > digit:
+            my_list.append(digit)
+        elif my_list[el] > digit and my_list[el + 1] < digit:
+            my_list.insert(el + 1, digit)
+    print(f"текущий список - {my_list}")
+    digit = int(input("Введите число (555 -> выход) "))
