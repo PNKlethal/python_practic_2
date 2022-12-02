3e1a6b9023c325c3dc34c33ac787eb839d1cf6a6
PNKlethal <seregaa99@bk,ru>
2022-12-02 21:00:11 +0600

    6 задание, есть ошибка list object is not callable но всё вроде работает, не совсем понимаю пока, что это значит

--- main.py
+++ main.py
@@ -71,3 +71,35 @@ while digit != 555:
             my_list.insert(el + 1, digit)
     print(f"текущий список - {my_list}")
     digit = int(input("Введите число (555 -> выход) "))
+
+#6 задание. Реализовать структуру данных «Товары». Она должна представлять собой список кортежей. Каждый кортеж хранит информацию об отдельном товаре. В кортеже должно быть два элемента — номер товара и словарь с параметрами (характеристиками товара: название, цена, количество, единица измерения). Структуру нужно сформировать программно, т.е. запрашивать все данные у пользователя.
+
+products = []
+for i in range(1, 4):
+    print(f"Заполняем информацию по {i}-му товару")
+    product_name = input("Название: ")
+    product_price = str(input("Цена: "))
+    product_count = int(input("Количество: "))
+    product_measure =  input("Единица измерения: ")
+    products.append((i, {'название': product_name, 'цена': product_price, 'количество': product_count, 'eд': product_measure}))
+
+print(f"Исходный список товаров: \n{products}")
+
+product_names = []
+product_prices = []
+product_counts = []
+product_measures = []
+for i in products:
+    product_names.append(i[1].get('название'))
+    product_prices.append(i[1].get('цена'))
+    product_counts.append(i[1].get('количество'))
+    product_measures.append(i[1].get('eд'))
+
+report = {
+    'название': list(set(product_names)),
+    'цена': list(set(product_prices)),
+    'количество': list(set(product_counts)),
+    'eд': list(set(product_measures))
+}
+
+print(f"Отчет по списку товаров: \n{report}")
